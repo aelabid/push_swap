5464a237c66252096001be933e7733164085dc1f
Asmaa Elabid <aelabid@e1r4p4.1337.ma>
2022-03-18 16:15:57 +0100

    delete all

--- test.py
+++
@@ -1,8 +0,0 @@
-import itertools
-import subprocess
-
-c = itertools.permutations("12345", 5)
-for i in c:
-        p1 = subprocess.getoutput(f"./push_swap {' '.join(i)} | ./checker {' '.join(i)}")
-        p2 = subprocess.getoutput(f"./push_swap {' '.join(i)} | wc -l")
-        print(p1 + ": " + p2)
