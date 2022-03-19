adaf356593052a8e52827bb2b672a59e2b2560c0
Asmaa Elabid <aelabid@e1r5p4.1337.ma>
2022-03-19 14:09:54 +0100

    push_swap

--- pushswap_util.c
+++ pushswap_util.c
@@ -6,7 +6,7 @@
 /*   By: aelabid <aelabid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 14:43:23 by aelabid           #+#    #+#             */
-/*   Updated: 2022/03/14 17:06:57 by aelabid          ###   ########.fr       */
+/*   Updated: 2022/03/19 14:09:19 by aelabid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,6 @@ size_t	ft_strlen2(const char *s)
 
 void	error(void)
 {
-	ft_putstr("Error");
+	ft_putstr("Error\n");
 	exit(1);
 }
